8ded41f4c086a9777f09649ddbf1382c3da1edcb
llopez020 <89426021+llopez020@users.noreply.github.com>
2023-05-16 22:48:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ io.on('connection', function (sockett) {
 app.post("*/data", function(req, res){
     var newUser = new User(req.body.data);
 	
-	User.findOne({user: newUser.user}, function(err, docs) {
+	User.find().then({user: newUser.user}, function(err, docs) {
 		if(!docs){
 			newUser.wins = 0;
 			newUser.password = bcrypt.hashSync(newUser.password, 10);
@@ -92,7 +92,7 @@ app.post("*/data", function(req, res){
 app.post("*/login", function(req,res){
 	var tempUser = new User(req.body.data);
 
-	User.findOne({user: tempUser.user}, function(err, docs) {
+	User.find().then({user: tempUser.user}, function(err, docs) {
 		if(docs){
 			bcrypt.compare(tempUser.password, docs.password, function(err, result) {
 				if(result==true) {
